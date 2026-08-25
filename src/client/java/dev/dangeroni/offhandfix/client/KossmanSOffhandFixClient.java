0cca95371962b839ecdb441fbbafb71125514703
Kossman <an.stawski@outlook.com>
2026-08-25 22:28:54 +0300

    chore: init mod with Fabric template

---
+++ src/client/java/dev/dangeroni/offhandfix/client/KossmanSOffhandFixClient.java
@@ -0,0 +1,10 @@
+package dev.dangeroni.offhandfix.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KossmanSOffhandFixClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
